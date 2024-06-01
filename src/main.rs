c732428cc63fd513f4d068623a674933a44cf0fd
HMacN <hugh.macn@gmail.com>
2024-06-01 17:46:21 +0100

    Initial setup of the project, including: getting a placeholder test running, and adding a 'main' function to help with manual testing later.

---
+++ src/main.rs
@@ -0,0 +1,7 @@
+use ember;
+
+fn main()
+{
+    /* Code to help manually test visual display elements that can't be tested using the tests in
+    the tests folder is to go here. */
+}
---
+++ tests/initial_tests/initial_tests.rs
@@ -0,0 +1,8 @@
+use ember::add;
+
+#[test]
+fn placeholder_test()
+{
+    let result = add(2, 2);
+    assert_eq!(result, 4);
+}
