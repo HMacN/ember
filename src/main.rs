878b8ffd8a7a290fa587fd06978248d5148b9757
Hugh <hugh.macn@gmail.com>
2025-06-12 23:45:23 +0100

    Added links to possible 2d and 3d rendering crates.

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,15 @@ fn main()
 {
     /* Code to help manually test visual display elements that can't be tested using the tests in
     the tests folder is to go here. */
+    
+    // https://crates.io/crates/pixels
+    
+    // https://crates.io/crates/miniquad
+    
+    // https://crates.io/crates/three-d
+    // Looks like it might be the best option
+    
+    
 
     let mut test_window = EmberWindow::new();
     test_window.run();
